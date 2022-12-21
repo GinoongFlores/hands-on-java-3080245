36c84311c0a8cfa53675ce00b8c52abbae5b2c5e
Christian Paul Flores <florespaul161@gmail.com>
2022-12-21 00:18:51 +0000

    fix typo on db_file

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DataSource {
   
   public static Connection connect() {
-    String db_file = "jdbc:sqlite:resources/bank.bd";
+    String db_file = "jdbc:sqlite:resources/bank.db";
     Connection connection = null; 
 
     try {
