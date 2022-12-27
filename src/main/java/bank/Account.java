70923360146f98720e0cdca3aeb7a702053864e3
Christian Paul Flores <florespaul161@gmail.com>
2022-12-27 12:29:57 +0000

    Update data source

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -37,12 +37,15 @@ public class Account {
     this.balance = balance;
   }
 
-  public void deposit(double amount) throws AmountException{
+  public void deposit(double amount) throws AmountException {
     if(amount < 1) {
       throw new AmountException("The minimum deposit is 1.00");
     } else {
       double newBalance = balance + amount;
       setBalance(newBalance);
+      
+      // Update the database.Implementing data persistent within an external resource.
+      DataSource.updateAccountBalance(id, newBalance);
     }
 
   }
