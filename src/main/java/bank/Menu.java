ce8a860c5ae7df1138a0c3f0a30a337f8ff6070c
Christian Paul Flores <florespaul161@gmail.com>
2022-12-27 11:13:15 +0000

    Display menu options
    creating menu options and added deposit & withdrawal method with amount parameter on Account.java

--- src/main/java/bank/Menu.java
+++ src/main/java/bank/Menu.java
@@ -46,5 +46,55 @@ public class Menu {
 
   public void showMenu(Customer customer, Account account) {
     
+    int selection = 0;
+
+    /* 
+     * User interfaces are 
+     * 1. Deposit 
+     * 2. Withdraw
+     * 3. Check Balance
+     * 4. Exit
+     */
+
+    // We want to let the user to make as many transaction as they want to. Therefore, the best option is a while loop. To keep the user in the menu until they choose to exit.
+
+    while(selection != 4 && customer.isAuthenticated()) {
+      System.out.println("==================================");
+      System.out.println("Please select an option: ");
+
+      System.out.println("1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit");
+      System.out.println("==================================");
+
+      selection = scanner.nextInt();
+      double amount = 0;
+
+      switch(selection) {
+        case 1:
+          System.out.println("How much you would like to deposit?");
+          amount = scanner.nextDouble();
+          account.deposit(amount);
+          break;
+
+        case 2: 
+          System.out.println("How much you would like to withdraw?");
+          amount = scanner.nextDouble();
+          account.withdraw(amount);
+          break;  
+        
+        case 3: 
+          System.out.println("Current balance: " + account.getBalance());
+          break;
+
+        case 4: 
+          Authenticator.logout(customer);
+          System.out.println("Thanks for banking at Flores Bank!");
+        
+        default: 
+          System.out.println("Invalid selection. Please try again.");
+
+      }
+
+    }
+
   }
 }
